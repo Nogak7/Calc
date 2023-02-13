a5844a052c9a44618f9333ec1d01bc4d44aae3bb
User <User@LAB3-15>
2023-02-13 09:48:38 +0200

    13.2

--- Calc/MainPage.xaml.cs
+++ Calc/MainPage.xaml.cs
@@ -2,36 +2,39 @@
 
 public partial class MainPage : ContentPage
 {
-	double clickedNum = 0;
-	int count = 0;
-	double clickedNum2 = 0;
-	string happend = "";
-	bool doti = false;
+    double clickedNum = 0;
+    int count = 0;
+    double clickedNum2 = 0;
+    string happend = "";
+    bool doti = false;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		
-	}
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
 
-	private void per_Clicked(object sender, EventArgs e)
-	{
+    }
 
-	}
+    private void per_Clicked(object sender, EventArgs e)
+    {
+        happend = "per";
+        clickedNum2 = clickedNum;
+        resolt.Text = "";
+        clickedNum = 0;
+    }
 
-	private void ac_Clicked(object sender, EventArgs e)
-	{
-		resolt.Text = "";
-		clickedNum = 0;
-		clickedNum2 = 0;
-	}
+    private void ac_Clicked(object sender, EventArgs e)
+    {
+        resolt.Text = "";
+        clickedNum = 0;
+        clickedNum2 = 0;
+    }
     private void Num_Clicked(object sender, EventArgs e)
     {
-		Button num = (Button)sender;
+        Button num = (Button)sender;
         if (clickedNum != 0)
         {
             if (doti)
@@ -51,64 +54,98 @@ public partial class MainPage : ContentPage
             }
         }
         if (clickedNum == 0)
-		{
-			if (doti)
-			{
+        {
+            if (doti)
+            {
 
-				clickedNum =  double.Parse(num.Text);
-				resolt.Text += clickedNum.ToString();
-				clickedNum = clickedNum2 + clickedNum / 10;
-				resolt.Text = clickedNum.ToString();
-				doti = false;
-			}
-			else
-			{
-				clickedNum =  double.Parse(num.Text);
-				resolt.Text += clickedNum.ToString();
-			}
-		}
-        
+                clickedNum = double.Parse(num.Text);
+                resolt.Text += clickedNum.ToString();
+                clickedNum = clickedNum2 + clickedNum / 10;
+                resolt.Text = clickedNum.ToString();
+                doti = false;
+            }
+            else
+            {
+                clickedNum = double.Parse(num.Text);
+                resolt.Text += clickedNum.ToString();
+            }
+        }
+
     }
 
 
 
-	private void div_Clicked(object sender, EventArgs e)
-	{
-		
-	}
+    private void div_Clicked(object sender, EventArgs e)
+    {
+        happend = "div";
+        clickedNum2 = clickedNum;
+        resolt.Text = "";
+        clickedNum = 0;
+    }
 
-	private void mul_Clicked(object sender, EventArgs e)
-	{
+    private void mul_Clicked(object sender, EventArgs e)
+    {
+        happend = "mul";
+        clickedNum2 = clickedNum;
+        resolt.Text = "";
+        clickedNum = 0;
+    }
 
-	}
+    private void sub_Clicked(object sender, EventArgs e)
+    {
+        happend = "sub";
+        clickedNum2 = clickedNum;
+        resolt.Text = "";
+        clickedNum = 0;
 
-	private void sub_Clicked(object sender, EventArgs e)
-	{
-		Button num = (Button)sender;
-        if (happend == "add")
-		{
-			double resolti = clickedNum2 + clickedNum;
-			resolt.Text = $"{resolti}";
-		}
-		
-		
-	}
+    }
 
-	private void add_Clicked(object sender, EventArgs e)
-	{
-		happend = "add";
-        Button num = (Button)sender;
-		clickedNum2 = clickedNum;
-		resolt.Text = "";
-		clickedNum = 0;
-	}
+    private void add_Clicked(object sender, EventArgs e)
+    {
+        happend = "add";
+        clickedNum2 = clickedNum;
+        resolt.Text = "";
+        clickedNum = 0;
+    }
 
-	private void dot_Clicked(object sender, EventArgs e)
-	{
-		doti = true;
+    private void dot_Clicked(object sender, EventArgs e)
+    {
+        doti = true;
         clickedNum2 = clickedNum;
         clickedNum = 0;
-        
+
+    }
+
+    private void eq_Clicked(object sender, EventArgs e)
+    {
+        Button num = (Button)sender;
+        if (happend == "add")
+        {
+            double resolti = clickedNum2 + clickedNum;
+            resolt.Text = $"{resolti}";
+
+        }
+        if (happend == "div")
+        {
+            double resolti = clickedNum2 / clickedNum;
+            resolt.Text = $"{resolti}";
+        }
+        if (happend == "mul")
+        {
+            double resolti = clickedNum2 * clickedNum;
+            resolt.Text = $"{resolti}";
+        }
+        if (happend == "sub")
+        {
+            double resolti = clickedNum2 - clickedNum;
+            resolt.Text = $"{resolti}";
+
+        }
+        if (happend == "per")
+        {
+            double resolti = clickedNum2/100 * clickedNum;
+            resolt.Text = $"{resolti}";
+        }
     }
 }
 
